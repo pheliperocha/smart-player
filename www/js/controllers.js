94873246853a9ef1fe03081fa49df86984ec0cac
Phelipe Rocha <phelipeafonso@gmail.com>
2016-03-24 11:07:56 -0300

    Initial Commit

---
+++ www/js/controllers.js
@@ -0,0 +1,9 @@
+angular.module('starter.controllers', [])
+
+.controller('PlaylistCtrl', function($scope) {})
+
+.controller('AlbunsCtrl', function($scope) {})
+
+.controller('ArtistsCtrl', function($scope) {})
+
+.controller('MusicCtrl', function($scope) {});
